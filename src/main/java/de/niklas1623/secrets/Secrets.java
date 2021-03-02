78dfb54789b0f652043c5748956aeccf00d7d1cd
niklas1623 <71941503+niklas1623@users.noreply.github.com>
2021-03-02 17:19:37 +0100

    Initial commit

---
+++ src/main/java/de/niklas1623/secrets/Secrets.java
@@ -0,0 +1,17 @@
+package de.niklas1623.secrets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Secrets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
